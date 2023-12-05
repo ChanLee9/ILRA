8f7c35d058f62aaa05a513f2729b0c9e04b8dd9e
chan <17612872708@163.com>
2023-12-05 20:07:13 +0800

    ss

--- preprocess.py
+++ preprocess.py
@@ -4,21 +4,22 @@ import os
 from sklearn.model_selection import train_test_split
   
 def read_data(data_dir, dataset_name):
+    train_path = os.path.join(data_dir, "train.tsv")
+    test_path = os.path.join(data_dir, "dev.tsv")
+    dev_path = os.path.join(data_dir, "test.tsv")
     if dataset_name == "RTE":
-        train_path = os.path.join(data_dir, "train.tsv")
-        test_path = os.path.join(data_dir, "dev.tsv")
-        dev_path = os.path.join(data_dir, "test.tsv")
         train_data = pd.read_csv(train_path, sep='\t', header=0)
         test_data = pd.read_csv(test_path, sep='\t', header=0)
         dev_data = pd.read_csv(dev_path, sep='\t', header=0)
         
     elif dataset_name == "CoLA":
-        train_path = os.path.join(data_dir, "train.tsv")
-        test_path = os.path.join(data_dir, "dev.tsv")
-        dev_path = os.path.join(data_dir, "test.tsv")
         train_data = pd.read_csv(train_path, sep='\t', header=None, names=['p1', 'label', 'p2', 'sentence'])
         test_data = pd.read_csv(test_path, sep='\t', header=None, names=['p1', 'label', 'p2', 'sentence'])
         dev_data = pd.read_csv(dev_path, sep='\t', header=None, names=['p1', 'label', 'p2', 'sentence'])
-    elif dataset_name == "MRPC":
-        split_mrpc_data(data_dir)
         
+    elif dataset_name == "MRPC":
+        train_data = pd.read_csv(train_path, sep='\t', header=0)
+        test_data = pd.read_csv(test_path, sep='\t', header=0)
+        dev_data = pd.read_csv(dev_path, sep='\t', header=0)
+        
+
