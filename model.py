4178e29115e362e43c558eafa6fdd140da25ee8e
chan <17612872708@163.com>
2023-12-07 21:53:36 +0800

    ss

--- model.py
+++ model.py
@@ -1,7 +1,6 @@
 import pandas as pd
 import torch
 import torch.nn as nn
-from torch.nn import CrossEntropyLoss
 import torch.functional as F
 from transformers import RobertaModel, AutoTokenizer
 
@@ -10,4 +9,7 @@ class MyModel(nn.Module):
         super().__init__()
         self.base_model = RobertaModel.from_pretrained(config.model_name_or_path)
         self.fc = nn.Linear(768, config.num_tags)
+    
+    def loss_fn():
+        criterion = nn.BCELoss()
         
