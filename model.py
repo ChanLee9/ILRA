418ad16d9c788be593a56930fe7c9e488504db99
chan <17612872708@163.com>
2023-12-08 20:20:56 +0800

    test for full finetuning

--- model.py
+++ model.py
@@ -4,12 +4,46 @@ import torch.nn as nn
 import torch.functional as F
 from transformers import RobertaModel, AutoTokenizer
 
-class MyModel(nn.Module):
+class NLU_Model(nn.Module):
     def __init__(self, config) -> None:
         super().__init__()
+        self.tokenizer = AutoTokenizer.from_pretrained(config.model_name_or_path)
         self.base_model = RobertaModel.from_pretrained(config.model_name_or_path)
-        self.fc = nn.Linear(768, config.num_tags)
+        self.fc = nn.Linear(self.base_model.pooler.dense.weight.shape[1], config.num_tags)
+        self.device = config.device
+        self.max_length = config.max_length
     
-    def loss_fn():
-        criterion = nn.BCELoss()
-        
+    def loss_fn(self, item):
+        criterion = nn.CrossEntropyLoss()
+        y_true = torch.LongTensor(item['label']).to(self.device)
+        logits, y_pred = self.forward(item)
+        loss = criterion(logits, y_true)
+        return loss
+                
+    def forward(self, item):
+        sentences1 = item['sentence1']    
+        if "sentence2" in item.keys():
+            sentences2 = item['sentence2']
+            text_encodings = self.tokenizer(sentences1,
+                                            sentences2,
+                                            truncation=True,
+                                            padding=True,
+                                            max_length=self.max_length,
+                                            return_tensors='pt')
+        else:
+            text_encodings = self.tokenizer(sentences1,
+                                            truncation=True,
+                                            padding=True,
+                                            max_length=self.max_length,
+                                            return_tensors='pt')
+        text_encodings = text_encodings.to(self.device)
+        logits = self.base_model(**text_encodings)
+        logits = self.fc(logits.pooler_output)
+        y_pred = torch.argmax(logits, dim=1)
+        return logits, y_pred
+            
+
+class NLG_Model(nn.Module):
+    def __init__(self, config) -> None:
+        super().__init__()
+    
--- training.py
+++ training.py
@@ -2,6 +2,7 @@ import json
 import pandas as pd
 import numpy as np
 import torch
+import loralib as lora
 from tqdm import tqdm
 import torch.nn as nn
 from transformers import get_scheduler
@@ -10,6 +11,7 @@ from transformers import RobertaModel, AutoTokenizer
 from sklearn.metrics import classification_report
 
 from preprocess import *
+from model import *
 
 WEIGHT_DECAY = 1e-2
 
@@ -23,7 +25,7 @@ def get_optimizer(model, dataloader, config):
     )
     return optimizer, lr_scheduler
     
-def train_loop(dataloader, model, optimizer, lr_scheduler, epoch, config):     # 一轮训练
+def train_loop(dataloader, model, optimizer, lr_scheduler, epoch):     # 一轮训练
     total_loss = 0.
     progress_bar = tqdm(range(len(dataloader)))
     progress_bar.set_description(f'epoch: {epoch}, loss: {0:>4f}')
@@ -43,21 +45,21 @@ def train_loop(dataloader, model, optimizer, lr_scheduler, epoch, config):     #
     return total_loss
 
 
-def dev_loop(config, dataloader, model):
+def dev_loop(dataloader, model):
     progress_bar = tqdm(range(len(dataloader)))
     progress_bar.set_description(f'validating... ')
     model.eval()
     y_pred, y_true = [], []
     with torch.no_grad():
         for batch, item in enumerate(dataloader):
-            y_true.append(item['label'])
-            y_pred.append(model(item).cpu().numpy())
+            y_true.extend(item['label'])
+            y_pred.extend(model(item)[1].cpu().numpy())
             progress_bar.update(1)
-        print(f'dev result: {classification_report(y_true, y_pred)}')
+        print(f'dev result: \n {classification_report(y_true, y_pred)}')
     return y_pred
 
 
-def test_loop(config, dataloader, model):
+def test_loop(dataloader, model):
     progress_bar = tqdm(range(len(dataloader)))
     progress_bar.set_description(f'testing... ')
     model.eval()
@@ -67,25 +69,45 @@ def test_loop(config, dataloader, model):
             y_pred = model(item).cpu().numpy()
         return y_pred
 
-class MyModel(nn.Module):
-    def __init__(self, *args, **kwargs) -> None:
-        super().__init__(*args, **kwargs)
-    
+def print_trainable_params(model):
+    """打印模型可训练参数量占比
 
+    Args:
+        model (_type_): _description_
+
+    """
+    total_params = sum([p.numel() for p in model.parameters()])
+    trainable_params = sum([p.numel() for p in model.parameters() if p.requires_grad])
+    trainable_ratio = trainable_params / total_params
+    print(f'total parameters: {total_params} || trainable parameters: {trainable_params} || \
+            trainable ratio: {100*trainable_ratio:.2f}%'
+        )
+
+def apply_lora(base_model, config):
+    if config.task_type == "NLU":
+        modules_to_apply = config.modules_to_apply
+        
+
+def get_model(config):
+    if config.task_type == "NLU":
+        base_model = RobertaModel.from_pretrained(config.model_name_or_path)
+        model = NLU_Model(config).to(config.device)
+    return model
 
 if __name__ == "__main__":
     @dataclass
     class Config:
         dataset_name = "MRPC"
         batch_size = 4
-        max_length = 256
+        max_length = 512
         model_name_or_path = "../pretrained_models/roberta"
-        device = "cpu"
+        device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
         lr = 1e-5
         epochs = 3
+        num_tags = 2
+        task_type = "NLU"
     
     config = Config()
-    model = RobertaModel.from_pretrained(config.model_name_or_path)
     
     train, dev, test = read_data(config)
     train_dataset = MyDataset(config, train)
@@ -96,6 +118,10 @@ if __name__ == "__main__":
     test_dataloader = MyDataLoader(test_dataset, config, shuffle=False)
     dev_dataloader = MyDataLoader(dev_dataset, config, shuffle=False)
     
-    
+    model = get_model(config)
     optimizer, lr_scheduler = get_optimizer(model, train_dataloader, config)
+    
+    for epoch in range(config.epochs):
+        train_loop(train_dataloader, model, optimizer, lr_scheduler, epoch)
+        dev_loop(dev_dataloader, model)
     
